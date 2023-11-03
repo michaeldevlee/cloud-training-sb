b8c458d2db2527d59b53e0cfc53597f52136c5a2
mikelee53 <mikeleevt@gmail.com>
2023-11-03 14:04:02 -0700

    adjusted start command to discard stdout from starting .jar file

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar *.jar --spring.config.name=spring-boot-demo --spring.profiles.active=local &
+sudo java -jar *.jar --spring.config.name=spring-boot-demo --spring.profiles.active=local > /dev/null 2> /dev/null < /dev/null &
