84f9ad2917a4c1b7fa9b1433864e1a63ccb26fa1
mikelee53 <mikeleevt@gmail.com>
2023-11-03 13:52:53 -0700

    updated spring boot args, running as a background process in ec2

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8090 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar *.jar --spring.config.name=spring-boot-demo --spring.profiles.active=local &
